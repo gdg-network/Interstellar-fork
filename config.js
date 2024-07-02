c298b657dc045c910450169fe19ac38576eeb8b5
GDG-network <162386566+gdg-network@users.noreply.github.com>
2024-07-02 14:53:41 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,12 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
-  users: {
-    // You can add multiple users by doing username: 'password'.
-    interstellar: "password",
+  challenge: true, // Set to true if you want to enable password protection.
+  users: { // You can add multiple users by doing username: 'password'. 
+    interstellar: 'password',
+    liam: 'iscool',
+    lucas: 'madepass',
+
   },
-};
-export default config;
+  routes: true, // Change this to false if you just want to host a bare server.
+  local: true, // Change this to false to disable local assets.
+}
+export default config
